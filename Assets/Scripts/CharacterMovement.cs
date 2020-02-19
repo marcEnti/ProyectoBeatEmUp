f2f0b4f4203049002639e616e1897cdd1df83c70
marcRomera <marc.romera.rodriguez@enti.cat>
2020-02-20 00:35:56 +0100

    Change in Player Sprite, CharacterMovement.cs Added

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    [SerializeField] bool canPlayerMove = true;
+    [SerializeField] float speed = 5;
+    [SerializeField] GameObject player;
+
+    enum Direction { Up, Down, Left, Right }
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            MoveCharacter(Direction.Up);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveCharacter(Direction.Down);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MoveCharacter(Direction.Right);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveCharacter(Direction.Left);
+        }
+      
     }
+
+    void MoveCharacter(Direction direction)
+    {
+        switch(direction)
+        {
+            case Direction.Up:
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + (speed * 0.01f), player.transform.position.z);
+                break;
+            case Direction.Down:
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - (speed * 0.01f), player.transform.position.z);
+                break;
+            case Direction.Left:
+                player.transform.position = new Vector3(player.transform.position.x - (speed * 0.01f), player.transform.position.y, player.transform.position.z);
+                break;
+            case Direction.Right:
+                player.transform.position = new Vector3(player.transform.position.x + (speed * 0.01f), player.transform.position.y, player.transform.position.z);
+                break;
+            default:
+                break;
+        }
+    }
+        
 }
