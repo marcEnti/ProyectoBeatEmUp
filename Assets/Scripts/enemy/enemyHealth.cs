c544eb6a0a8a40be64ad3299225c769149d9f1b9
MarcEnti <marc.romera.rodriguez@enti.cat>
2020-02-26 20:20:29 +0100

    df

---
+++ Assets/Scripts/enemy/enemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    [SerializeField] float health = 20;
+    
+
+    BoxCollider2D collider2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+ 
+    }
+    
+
+}
---
+++ Assets/enemyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    enum EnemyState { STARTING, WANDERING, TRACKING, CHARGING, HITING, STUNNED, KNOCKBACK, DEATH };
+    EnemyState enemyState;
+    Rigidbody2D rb;
+    int enemySpeed = 3;
+    [SerializeField] GameObject player;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        enemyState = EnemyState.STARTING;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch(enemyState)
+        {
+            case EnemyState.STARTING:
+                
+                break;
+        }
+    }
+}
